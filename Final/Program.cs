a681665d3bb0d246488005f92a6cd51f08806082
Manal-Oraha <Manal.Oraha@outlook.com>
2026-09-15 20:03:24 +0300

    Update Program.cs

--- Final/Program.cs
+++ Final/Program.cs
@@ -17,9 +17,9 @@ class Program
             // Create a new student object
             var student = new Student
             {
-                FirstName = "Emad",
-                LastName = "Mohamad",
-                DateOfBirth = new DateTime(1985,11, 10)
+                FirstName = "Manal",
+                LastName = "Oraha",
+                DateOfBirth = new DateTime(1980,1, 10)
             };
 
             // Add the student to the Students DbSet
@@ -31,7 +31,7 @@ class Program
             Console.WriteLine("The Student is added successfully!");
 
             // Retrieve and display the student to verify
-            var savedStudent = context.Students.FirstOrDefault(s => s.FirstName == "Emad");
+            var savedStudent = context.Students.FirstOrDefault(s => s.FirstName == "Manal");
             if (savedStudent != null)
             {
                 Console.WriteLine($" Student: {savedStudent.FirstName} {savedStudent.LastName}");
